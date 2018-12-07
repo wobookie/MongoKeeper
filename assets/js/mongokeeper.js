de83a540360919767658aa99ad40c32b44d1989c
Hansolo <thiemo.heims@icloud.com>
2018-12-07 21:48:42 +0800

    Login and Count
    Adjusted  login according to sample and added function to count documents in collection. Next steps - integrate upload function and do recount after upload.
    
    Co-Authored-By: Viktor Kessler <vikkes@users.noreply.github.com>

--- assets/js/mongokeeper.js
+++ assets/js/mongokeeper.js
@@ -1,41 +1,63 @@
 // Get references to page elements
 const statusMessage = document.getElementById("statusMessageId");
 const loginMessage = document.getElementById("loginMessageId");
+const loginErrorMessage = document.getElementById("loginErrorId")
+const collectionMessage = document.getElementById("collectionMessageId");
+
 
 // Set event listener
 
 
 // Setup MongoDB Stitch
-const appID = "archive-jeqag";
+const {
+    Stitch,
+    RemoteMongoClient,
+    UserPasswordCredential,
+    UserPasswordAuthProviderClient
+} = stitch;
+
+
+const appID = "archive-jeqag"; 
 const stitchClient = stitch.Stitch.initializeDefaultAppClient(appID);
+const db = stitchClient.getServiceClient(stitch.RemoteMongoClient.factory, 'mongodb-atlas').db('dwhArchive');
+const collection = db.collection("ingest")
+
 
 if (stitch.Stitch.hasAppClient(appID)) {
-    statusMessage.innerText = "Client for ID " + appID + " initialised."
+    statusMessage.innerText = "Client for ID " + appID + " initialised.";
 } else {
-    statusMessage.innerText = "Client for ID " + appID + " not initialised!"
+    statusMessage.innerText = "Client for ID " + appID + " not initialised!";
 }
 
 if (stitchClient.auth.isLoggedIn) {
-    hideLoginForm();
+    hideLoginContainer();
     revealDashboardContainer();
-    build(Date.now());
 }
 
 async function handleLogin() {
+    console.log("Handle login - getLoginForm");
     const { email, password } = getLoginFormInfo();
     await emailPasswordAuth(email, password);
-    build(Date.now());
 }
 
 // Authenticate with Stitch as an email/password user
 async function emailPasswordAuth(email, password) {
+    console.log("Handle login request via eMailPasswordAuth");
+
     if (!stitchClient.auth.isLoggedIn) {
         // Log the user in
         const credential = new UserPasswordCredential(email, password);
-        await stitchClient.auth.loginWithCredential(credential);
+        stitchClient.auth.loginWithCredential(credential).then(authedId => {
+		console.log('successfully logged in with id: ${authedId}');
+		hideLoginContainer();
+		revealDashboardContainer();
+		countAllDocuments();
+	})
+	.catch(error => {
+		console.error('login failed ' + error);
+		loginErrorMessage.innerText = "Login Failed - Incorrect email / password !";
+	})
     }
-    hideLoginForm();
-    revealDashboardContainer();
 }
 
 function getLoginFormInfo() {
@@ -56,9 +78,22 @@ function revealDashboardContainer() {
 }
 
 function hideLoginContainer() {
-    const container = document.getElementById("loginContainerId");
-    const user = stitchClient.auth.user;
+	const container = document.getElementById("loginContainerId");
+    	const user = stitchClient.auth.user;
 
-    container.classList.add("hidden");
-    loginMessage.innerText = "Logged in as: " + user.profile.data.email;
+    	container.classList.add("hidden");
+    	loginMessage.innerText = "Logged in as: " + user.profile.data.email;
+}
+
+function countAllDocuments() {
+	return (collection.count());
+}
+
+async function refreshStatus() {
+	try {
+		var documentCount = await countAllDocuments();
+		collectionMessage.innerText = "Found " + documentCount + " documents in collection";
+	} catch(error) {
+		console.error(error);
+	}
 }
